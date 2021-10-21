3e246abd4ce1007b36c2628f4dd273bb7b8a4c5c
Justin Anderson <justin@watchworddigital.com>
2021-10-21 15:40:12 -0700

    WIP documentation

--- src/Pet.js
+++ src/Pet.js
@@ -2,7 +2,7 @@ import Modal from "./Modal";
 import Details from "./Details";
 import { useState } from "react";
 
-export default function Pet({ age, name, heroPic, location, id }) {
+export default function Pet({ age, name, size, breed, heroPic, location, id }) {
   const [showModal, setShowModal] = useState(false);
 
   function handleToggleModal() {
@@ -20,7 +20,24 @@ export default function Pet({ age, name, heroPic, location, id }) {
       </div>
       <div className="info" onClick={handleToggleModal}>
         <h1>{name}</h1>
-        <h2>{`${age} grumpy cat in ${location}`}</h2>
+        <dl>
+          <div className="info-pair">
+            <dt>AGE</dt>
+            <dd>{age}</dd>
+          </div>
+          <div className="info-pair">
+            <dt>SIZE</dt>
+            <dd>{size}</dd>
+          </div>
+          <div className="info-pair">
+            <dt>BREED</dt>
+            <dd>{breed}</dd>
+          </div>
+          <div className="info-pair">
+            <dt>LOCATION</dt>
+            <dd>{location}</dd>
+          </div>
+        </dl>
       </div>
 
       {showModal ? (
